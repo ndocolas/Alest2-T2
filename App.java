58b05e23fa9966531af70b263b3148b33fe52872
Nicolas <salocod@icloud.com>
2024-06-06 16:52:12 -0300

    correcao escopo

--- App.java
+++ App.java
@@ -1,13 +1,13 @@
 public class App {
 
     public static void main(String[] args) {
-        System.out.println(new Leitura("tamanho_10.txt"));
-        System.out.println(new Leitura("tamanho_50.txt"));
-        System.out.println(new Leitura("tamanho_100.txt"));
-        System.out.println(new Leitura("tamanho_798.txt"));
-        System.out.println(new Leitura("tamanho_1000.txt"));
-        System.out.println(new Leitura("tamanho_5000.txt"));
-        System.out.println(new Leitura("tamanho_10000.txt"));
-        System.out.println(new Leitura("tamanho_50000.txt"));
+        String dir = "./arquivos/";
+        System.out.println(new Leitura(dir + "tamanho_10.txt"));
+        System.out.println(new Leitura(dir + "tamanho_50.txt"));
+        System.out.println(new Leitura(dir + "tamanho_100.txt"));
+        System.out.println(new Leitura(dir + "tamanho_798.txt"));
+        System.out.println(new Leitura(dir + "tamanho_1000.txt"));
+        System.out.println(new Leitura(dir + "tamanho_5000.txt"));
+        System.out.println(new Leitura(dir + "tamanho_10000.txt"));
     }
 }
