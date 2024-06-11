7eff3fdfec9a6c561d8348b1d9e97e01396e7b50
Nicolas <nicolausao@icloud.com>
2024-06-11 09:20:27 -0300

    teste

--- BuscaEmProfundidade.java
+++ BuscaEmProfundidade.java
@@ -8,25 +8,25 @@ public class BuscaEmProfundidade {
     public BuscaEmProfundidade(Grafo grafo) {this.grafo = grafo;}
 
     public int encontrarCaminhoMaisLongo() {
-        int[] dp = new int[grafo.getNumVertices()];
-        Arrays.fill(dp, -1);
+        int[] lista = new int[grafo.getNumVertices()];
+        Arrays.fill(lista, -1);
 
         return IntStream.range(0, grafo.getNumVertices())
-                        .map(i -> buscaEmProfundidade(this.grafo, i, dp))
+                        .map(i -> buscaEmProfundidade(this.grafo, i, lista))
                         .max()
                         .orElse(0);
     }
 
-    private int buscaEmProfundidade(Grafo grafo, int v, int[] dp) {
-        if (dp[v] != -1) return dp[v];
+    private int buscaEmProfundidade(Grafo grafo, int v, int[] lista) {
+        if (lista[v] != -1) return lista[v];
 
         int comprimentoMax;
         comprimentoMax = 1 + grafo.adjacentes(v).stream()
-                                  .mapToInt(vizinho -> buscaEmProfundidade(grafo, vizinho, dp))
+                                  .mapToInt(vizinho -> buscaEmProfundidade(grafo, vizinho, lista))
                                   .max()
                                   .orElse(0);
 
-        dp[v] = comprimentoMax;
+        lista[v] = comprimentoMax;
         return comprimentoMax;
     }
     
