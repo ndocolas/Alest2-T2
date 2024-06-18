be15b05dd886596ac2bb80d41b4ff7d54c2a1484
Nicolas <nicolas.docolas1804@gmail.com>
2024-06-18 10:54:07 -0300

    correcao e implementacao da classe App e correcoes gerais

--- App.java
+++ App.java
@@ -1,25 +1,27 @@
-public class App{
+public class App {
 
     public static void main(String[] args) {
         String dir = "./arquivos/";
-        String[] leitura = {
-        dir + "caixas_5.txt",
-        dir + "caixas_11.txt",
-        dir + "caixas_12.txt",
-        dir + "caixas_15.txt",
-        dir + "tamanho_10.txt", 
-        dir + "tamanho_20.txt",
-        dir + "tamanho_50.txt",
-        dir + "tamanho_100.txt",
-        dir + "tamanho_200.txt",
-        dir + "tamanho_300.txt",
-        dir + "tamanho_1000.txt",
-        dir + "caixas_1005.txt",
-        dir + "tamanho_2000.txt",
-        dir + "tamanho_10000.txt",};
+        String[] ler = {
+            dir + "caixas_5" + ".txt",
+            dir + "caixas_11" + ".txt",
+            dir + "caixas_12" + ".txt",
+            dir + "caixas_15" + ".txt",
+            dir + "tamanho_10" + ".txt",
+            dir + "tamanho_20" + ".txt",
+            dir + "tamanho_50" + ".txt",
+            dir + "tamanho_100" + ".txt",
+            dir + "tamanho_200" + ".txt",
+            dir + "tamanho_300" + ".txt",
+            dir + "tamanho_1000" + ".txt",
+            dir + "caixas_1005" + ".txt",
+            dir + "tamanho_2000" + ".txt",
+            dir + "tamanho_10000" + ".txt"
+        };
 
-        for (String str : leitura) {
+        for (String str : ler) {
             System.out.println(new Leitura(str));
         }
     }
+    
 }
