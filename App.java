767eb08c824fe8f924660f8f773e24d363ad4d31
Nicolas <nicolas.docolas1804@gmail.com>
2024-06-19 20:27:55 -0300

    remocao do import acima da classe

--- App.java
+++ App.java
@@ -1,23 +1,21 @@
-import java.util.Arrays;
-
 public class App {
 
     public static void main(String[] args) {
-        Arrays.asList(
+        java.util.Arrays.asList(
                 "caixas_5",
-                 "caixas_11",
-                 "caixas_12",
-                 "caixas_15",
-                 "tamanho_10",
-                 "tamanho_20",
-                 "tamanho_50",
-                 "tamanho_100",
-                 "tamanho_200",
-                 "tamanho_300",
-                 "tamanho_1000",
-                 "caixas_1005",
-                 "tamanho_2000",
-                 "tamanho_10000")
+                "caixas_11",
+                "caixas_12",
+                "caixas_15",
+                "tamanho_10",
+                "tamanho_20",
+                "tamanho_50",
+                "tamanho_100",
+                "tamanho_200",
+                "tamanho_300",
+                "tamanho_1000",
+                "caixas_1005",
+                "tamanho_2000",
+                "tamanho_10000")
 
 		    .stream().map(str -> "./arquivos/" + str + ".txt")
         	.forEach(arq -> System.out.println(new Leitura(arq)));
