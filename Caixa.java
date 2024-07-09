04db92b9d15326e08ca94db13126271cf74a01f5
Nicolas <nicolas.docolas1804@gmail.com>
2024-07-09 14:49:09 -0300

    imeplementacao em record

--- Caixa.java
+++ Caixa.java
@@ -1,18 +1,4 @@
-import java.util.Arrays;
-import java.util.stream.IntStream;
-
-public class Caixa implements Comparable<Caixa> {
-    
-    private final int id;
-    private final int[] dimensoes;
-
-    public Caixa(int id, int[] dimensoes) {
-        this.id = id;
-        this.dimensoes = dimensoes;
-    }
-
-    @Override
-    public int compareTo(Caixa outra) {return Arrays.compare(this.dimensoes, outra.dimensoes);}
-    public boolean cabeDentro(Caixa outra) {return IntStream.range(0, 3).allMatch(i -> this.dimensoes[i] < outra.dimensoes[i]);}
-    public int getId() {return id;}
+public record Caixa (int id, int[] dimensoes) implements Comparable<Caixa> {
+    @Override public int compareTo(Caixa outra) {return java.util.Arrays.compare(this.dimensoes, outra.dimensoes());}
+    public boolean cabeDentro(Caixa outra) {return java.util.stream.IntStream.range(0, 3).allMatch(i -> this.dimensoes[i] < outra.dimensoes[i]);}
 }
