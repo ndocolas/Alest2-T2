e76148ad5ec829bdc09ee666d2b77ab1c129a943
Nicolas Docolas <145163721+ndocolas@users.noreply.github.com>
2024-07-09 14:53:44 -0300

    correção espaço

--- App.java
+++ App.java
@@ -15,7 +15,7 @@ public class App {
                 "caixas_1005",
                 "tamanho_2000",
                 "tamanho_10000")
-		        .stream().map(str -> "./arquivos/" + str + ".txt")
+		.stream().map(str -> "./arquivos/" + str + ".txt")
                 .forEach(arq -> System.out.println(new Leitura(arq)));
     }
 }
